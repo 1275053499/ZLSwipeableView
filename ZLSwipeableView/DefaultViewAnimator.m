71ea81faab6f62a223215a89757b98bbfefe6ddc
selice <selice_lin@126.com>
2022-02-17 09:13:59 +0800

    Update DefaultViewAnimator.m

--- ZLSwipeableView/DefaultViewAnimator.m
+++ ZLSwipeableView/DefaultViewAnimator.m
@@ -43,41 +43,87 @@ atOffsetFromCenter:(CGPoint)offset
               index:(NSUInteger)index
               views:(NSArray<UIView *> *)views
       swipeableView:(ZLSwipeableView *)swipeableView {
-    CGFloat degree = 1;
-    NSTimeInterval duration = 0.4;
-    CGPoint offset = CGPointMake(0, CGRectGetHeight(swipeableView.bounds) * 0.3);
-    switch (index) {
-    case 0:
-        [self rotateView:view
-                     forDegree:0
-                      duration:duration
-            atOffsetFromCenter:offset
-                 swipeableView:swipeableView];
-        break;
-    case 1:
-        [self rotateView:view
-                     forDegree:degree
-                      duration:duration
-            atOffsetFromCenter:offset
-                 swipeableView:swipeableView];
-        break;
-    case 2:
-        [self rotateView:view
-                     forDegree:-degree
-                      duration:duration
-            atOffsetFromCenter:offset
-                 swipeableView:swipeableView];
-        break;
-    case 3:
-        [self rotateView:view
-                     forDegree:0
-                      duration:duration
-            atOffsetFromCenter:offset
-                 swipeableView:swipeableView];
-        break;
-    default:
-        break;
-    }
+      
+      switch (index) {
+        case 0:
+            {
+                [UIView animateWithDuration:0.5 animations:^{
+                    view.transform = CGAffineTransformMakeScale(1, 1);
+                                } completion:^(BOOL finished) {
+                                    
+                                }];
+               
+            }
+            break;
+        case 1:
+            {
+                [UIView animateWithDuration:0.5 animations:^{
+                    view.transform = CGAffineTransformMakeScale(0.95, 0.95);
+                                } completion:^(BOOL finished) {
+                                    
+                                }];
+              
+            }
+            break;
+            case 2:{
+                [UIView animateWithDuration:0.5 animations:^{
+                    view.transform = CGAffineTransformMakeScale(0.90, 0.90);
+                                } completion:^(BOOL finished) {
+                                    
+                                }];
+            }
+               
+            break;
+            case 3:{
+                [UIView animateWithDuration:0.6 animations:^{
+                    view.transform = CGAffineTransformMakeScale(0.85, 0.85);
+                                } completion:^(BOOL finished) {
+                                    
+                                }];
+            }
+            break;
+            default:{
+                
+            }
+            break;
+        }
+        
+//         原来效果
+//     CGFloat degree = 1;
+//     NSTimeInterval duration = 0.4;
+//     CGPoint offset = CGPointMake(0, CGRectGetHeight(swipeableView.bounds) * 0.3);
+//     switch (index) {
+//     case 0:
+//         [self rotateView:view
+//                      forDegree:0
+//                       duration:duration
+//             atOffsetFromCenter:offset
+//                  swipeableView:swipeableView];
+//         break;
+//     case 1:
+//         [self rotateView:view
+//                      forDegree:degree
+//                       duration:duration
+//             atOffsetFromCenter:offset
+//                  swipeableView:swipeableView];
+//         break;
+//     case 2:
+//         [self rotateView:view
+//                      forDegree:-degree
+//                       duration:duration
+//             atOffsetFromCenter:offset
+//                  swipeableView:swipeableView];
+//         break;
+//     case 3:
+//         [self rotateView:view
+//                      forDegree:0
+//                       duration:duration
+//             atOffsetFromCenter:offset
+//                  swipeableView:swipeableView];
+//         break;
+//     default:
+//         break;
+//     }
 }
 
 @end
